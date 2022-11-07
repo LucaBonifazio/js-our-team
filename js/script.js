b4fb026a71ae45b63d5f665c9f09c532fbdb50a0
LucaBonifazio <luca.bonifazio13@gmail.com>
2022-11-07 14:42:00 +0100

    milestone 1

--- js/script.js
+++ js/script.js
@@ -14,3 +14,44 @@
 // Ragioniamo come sempre a step.
 // Prima la logica in italiano e poi traduciamo in codice.
 // E ricordiamoci che console.log() è nostro amico!
+
+const arrTeam = [
+    {
+        image: `img/wayne-barnett-founder-ceo.jpg`,
+        name: 'Wayne',
+        lastName: 'Barnett',
+        occupation: 'Founder & CEO',
+    },
+    {
+        image: `img/angela-caroll-chief-editor.jpg`,
+        name: 'Angela',
+        lastName: 'Caroll',
+        occupation: 'Chief Editor',
+    },
+    {
+        image: `img/walter-gordon-office-manager.jpg`,
+        name: 'Walter',
+        lastName: 'Gordon',
+        occupation: 'Office Manager',
+    },
+    {
+        image: `img/angela-lopez-social-media-manager.jpg`,
+        name: 'Angela',
+        lastName: 'Lopez',
+        occupation: 'Social Media Manager',
+    },
+    {
+        image: `img/scott-estrada-developer.jpg`,
+        name: 'Scott',
+        lastName: 'Estrada',
+        occupation: 'Developer',
+    },
+    {
+        image: `img/barbara-ramos-graphic-designer.jpg`,
+        name: 'Barbara',
+        lastName: 'Ramos',
+        occupation: 'Graphic Designer',
+    },
+]
+
+console.table(arrTeam);
